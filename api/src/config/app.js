cf29eca4573f42c62b394502df179bf6fabc251a
Thomas Barenghi <90881270+thomasbarenghi@users.noreply.github.com>
2023-05-18 00:47:57 -0300

    env back

--- api/src/config/app.js
+++ api/src/config/app.js
@@ -3,12 +3,15 @@ const morgan = require("morgan");
 const { Server } = require("socket.io");
 const http = require("http");
 const cors = require("cors");
+require("dotenv").config();
+
+const {CLIENT_URL} = process.env;
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: CLIENT_URL,
   },
 });
 
