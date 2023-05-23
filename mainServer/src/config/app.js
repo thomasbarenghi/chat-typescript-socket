5581950765b743e38c55a4b81247b41ff857626c
Thomas Barenghi <90881270+thomasbarenghi@users.noreply.github.com>
2023-05-23 17:02:54 -0300

    remover peer from main

--- mainServer/src/config/app.js
+++ mainServer/src/config/app.js
@@ -4,7 +4,7 @@ const http = require("http");
 const cors = require("cors");
 const router = require("../routes/index.js");
 require("dotenv").config();
-const { PeerServer, ExpressPeerServer } = require("peer");
+// const { PeerServer, ExpressPeerServer } = require("peer");
 const socketSetup = require("../socket/config.js");
 
 const app = express();
@@ -14,22 +14,22 @@ app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
 
-const peerServer = PeerServer({
-  debug: true,
-  path: "/app",
-  port: 9000,
-});
+// const peerServer = PeerServer({
+//   debug: true,
+//   path: "/app",
+//   port: 9000,
+// });
 
-peerServer.on("connection", (client) => {
-  console.log("Client connected", client.id);
-});
+// peerServer.on("connection", (client) => {
+//   console.log("Client connected", client.id);
+// });
 
-peerServer.on("disconnect", (client) => {
-  console.log("Client disconnected", client.id);
-});
+// peerServer.on("disconnect", (client) => {
+//   console.log("Client disconnected", client.id);
+// });
 
 app.use("/api", router);
-app.use("/peerjs", peerServer);
+//app.use("/peerjs", peerServer);
 socketSetup.attach(server);
 
 module.exports = { server };
